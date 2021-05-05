020696577a678deeade695e21fb3551a21d6695e
Sharon Nyambura Waithaka <Sharon.Waithaka@ngaocredit.com>
2021-05-05 16:31:32 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://yahoo.com"
           target="_blank"
           rel="noopener noreferrer"
         >
